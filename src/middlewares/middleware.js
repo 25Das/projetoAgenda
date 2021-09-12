347e74a94d490673d84c3ba1b4a7fbafee910117
Daniel Silva <daniel.raizon@gmail.com>
2021-09-12 06:14:48 -0300

    Reenable Helmet.

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -10,17 +10,17 @@ exports.outroMiddleware = (req, res, next) => {
 	next();
 };
 
-// exports.checkCsrfError = (err, req, res, next) => {
-// 	if (err) {
-// 		return res.render('404');
-// 	}
-// 	next();
-// };
+exports.checkCsrfError = (err, req, res, next) => {
+	if (err) {
+		return res.render('404');
+	}
+	next();
+};
 
-// exports.csrfMiddleware = (req, res, next) => {
-// 	res.locals.csrfToken = req.csrfToken();
-// 	next();
-// };
+exports.csrfMiddleware = (req, res, next) => {
+	res.locals.csrfToken = req.csrfToken();
+	next();
+};
 
 exports.loginRequired = (req, res, next) => {
 	if (!req.session.user) {
